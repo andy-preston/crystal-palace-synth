a3cbf5e6a18739c605f468068ece71125d7e2cf6
Andy Preston <edgeeffect@gmail.com>
2021-02-08 22:51:09 +0000

    pycodestyle fixes

--- build/generator.py
+++ build/generator.py
@@ -5,8 +5,8 @@ from math import sin
 testSineTable = open('./tables/testSine.inc', 'w')
 try:
     pi2 = 3.1415 * 2
-    size = 32 ** 2 # 32 bits
-    inc = pi2 / 1024 # 1024 elements
+    size = 32 ** 2  # 32 bits
+    inc = pi2 / 1024  # 1024 elements
     theta = 0.0
     testSineTable.write('testSineTable:')
     while (theta < pi2):
