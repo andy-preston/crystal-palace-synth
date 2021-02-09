99a15feaccd4dc40422513109cfa4599ff9bf022
Andy Preston <edgeeffect@gmail.com>
2021-02-09 12:45:40 +0000

    nicer formatting in table generator

--- build/generator.py
+++ build/generator.py
@@ -11,10 +11,19 @@ try:
     size = 2 ** 32  # 32 bits
     inc = pi2 / 1024  # 1024 elements
     theta = 0.0
+    words = 0
     while (theta < pi2):
-        testSineTable.write('    .4byte' + str(int(
+        words = words + 1
+        if words == 4:
+            testSineTable.write('\n')
+        testSineTable.write('    .4byte' if words == 0 else ',')
+        testSineTable.write(' ' + str(int(
             size * (sin(theta) + 1.0)
         )))
+        words = words + 1
+        if words == 4:
+            testSineTable.write('\n')
+            words = 0
         theta = theta + inc
 finally:
     testSineTable.close()
