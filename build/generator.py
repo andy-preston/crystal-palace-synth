6d4f7de356fd94cb91ac988c78171ad2daab068c
Andy Preston <edgeeffect@gmail.com>
2021-02-10 15:11:25 +0000

    fixed some issues with the sine table generator

--- build/generator.py
+++ build/generator.py
@@ -8,19 +8,21 @@ if not path.isdir('./tables'):
 testSineTable = open('./tables/testSine.inc', 'w')
 try:
     pi2 = pi * 2
-    size = 2 ** 32  # 32 bits
+    scale = ((2 ** 32) - 1) / 2  # 32 bits
     inc = pi2 / 1024  # 1024 elements
     theta = 0.0
     words = 0
     while (theta < pi2):
         testSineTable.write('    .4byte ' if words == 0 else ', ')
         testSineTable.write(str(int(
-            size * (sin(theta) + 1.0)
+            scale * (sin(theta) + 1.0)
         )))
         words = words + 1
         if words == 4:
             testSineTable.write('\n')
             words = 0
         theta = theta + inc
+    if words != 4:
+        testSineTable.write('\n')
 finally:
     testSineTable.close()
